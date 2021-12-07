358f323db0e63b645a1589966455e462205ccc7b
Hugh Carroll <hughie@carrollonline.co.uk>
2021-12-07 13:32:48 +0000

    Add rsync.

--- 40.base.sh
+++ 40.base.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 pacman -Syyy 
-pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-headers bluez bluez-utils cups alsa-utils pulseaudio pulseaudio-bluetooth git reflector xdg-utils xdg-user-dirs  
+pacman -S grub efibootmgr networkmanager network-manager-applet dialog mtools dosfstools base-devel linux-headers bluez bluez-utils cups alsa-utils pulseaudio pulseaudio-bluetooth git reflector xdg-utils xdg-user-dirs rsync 
 
 systemctl enable NetworkManager
 systemctl enable sshd
