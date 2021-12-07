b6b93e811b0297db83e76e614aa45beaf5fb118d
Hugh Carroll <hughie@carrollonline.co.uk>
2021-12-07 13:33:29 +0000

    Add i3gaps.

--- 70.i3.sh
+++ 70.i3.sh
@@ -4,5 +4,5 @@
 
 sudo reflector -c 'United Kingdom' -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syyy
-sudo pacman -S xorg xorg-xinit xf86-video-intel i3-wm i3lock i3status i3blocks dmenu alacritty neovim rofi ranger firefox lua nitrogen picom ntfs-3g
+sudo pacman -S xorg xorg-xinit xf86-video-intel i3-gaps i3lock i3status i3blocks dmenu alacritty neovim rofi ranger firefox lua nitrogen picom ntfs-3g
 
