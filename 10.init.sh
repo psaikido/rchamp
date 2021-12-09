6e2d11fc713c50f77446ed3b0dce5ef073713efe
Hugh Carroll <hughie@carrollonline.co.uk>
2021-12-09 10:19:35 +0000

    Comment out a redundancy.

--- 10.init.sh
+++ 10.init.sh
@@ -2,9 +2,12 @@
 
 loadkeys uk
 timedatectl set-ntp true
-read -p 'wifi name? ' wifiname
-read -p 'passphrase? ' wifipwd
-iwctl --passphrase $wifipwd station wlan0 connect $wifiname
+
+#connecting to the internet will already have to have happened in order to git clone this directory - here are the manual steps anyway
+#read -p 'wifi name? ' wifiname
+#read -p 'passphrase? ' wifipwd
+#iwctl --passphrase $wifipwd station wlan0 connect $wifiname
+
 reflector -c 'United Kingdom' -a 12 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Syyy
 
