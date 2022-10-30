82093ae0c572f7dae5faf656baaf7e4c62b9d654
Hugh Carroll <hughie@carrollonline.co.uk>
2022-10-30 16:18:44 +0000

    Update a few details.

--- scripts/on-metal/90.amp.sh
+++ scripts/on-metal/90.amp.sh
@@ -6,7 +6,7 @@ sudo systemctl enable httpd
 sudo systemctl enable mysql
 
 #sudo setfacl -m "u:http:--x" /home/hughie
-#chmod 775 -R ~/public_html
+#chmod 775 -R ~/sites/chanadmin
 
 #In /etc/httpd/conf/httpd.conf, comment the line:
 #LoadModule mpm_event_module modules/mod_mpm_event.so
@@ -15,15 +15,16 @@ sudo systemctl enable mysql
 #see https://wiki.archlinux.org/title/Apache_HTTP_Server#PHP
 #At the end of the 'LoadModule' list in the same file add:
 #LoadModule php_module modules/libphp.so
+#LoadModule php7_module modules/libphp7.so
 #AddHandler php-script .php
 
 #Place at the end of the include list
-#Include conf/extra/php_module.conf
+#Include conf/extra/php7_module.conf
 
 sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo mysql_secure_installation
 #edit /etc/php/php.ini
-#uncomment bz2, curl, iconf, mysqli, pdo_mysql
+#uncomment bz2, curl, iconv, mysqli, pdo_mysql
 #ALTER USER 'root'@'localhost' IDENTIFIED VIA mysql_native_password USING PASSWORD('root');
 #create /etc/httpd/conf/extra/phpmyadmin.conf
 # add
