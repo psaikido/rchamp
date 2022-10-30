76c683ad4828ff5f15ed97617188a074efdc17e0
Hugh Carroll <hughie@carrollonline.co.uk>
2022-10-30 02:31:30 +0000

    Script failed on buku so I took it out.

--- scripts/on-metal/70.gui.sh
+++ scripts/on-metal/70.gui.sh
@@ -4,5 +4,5 @@
 
 sudo reflector -c 'United Kingdom' -a 6 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syyy
-sudo pacman -S xorg xorg-xinit xf86-video-intel i3-gaps i3lock i3status i3blocks dmenu alacritty neovim rofi buku ranger firefox lua nitrogen picom ntfs-3g libdvdread fzf python3 ruby2.7 rubygems noto-fonts-cjk noto-fonts thunar pass
+sudo pacman -S xorg xorg-xinit xf86-video-intel i3-gaps i3lock i3status i3blocks dmenu alacritty neovim rofi ranger firefox lua nitrogen picom ntfs-3g libdvdread fzf python3 ruby2.7 rubygems noto-fonts-cjk noto-fonts thunar pass
 
