f5cec45bf0892dfbc54a931894b846ccf6cde1b3
Hugh Carroll <hughie@carrollonline.co.uk>
2022-11-01 16:25:08 +0000

    Set up the main config files.

--- scripts/on-metal/80.configs.sh
+++ scripts/on-metal/80.configs.sh
@@ -1,44 +1,46 @@
 #!/bin/bash
 
-git clone https://bitbucket.org/psaikido/dotfiles
+#git clone https://bitbucket.org/psaikido/dotfiles
+#git clone https://gitlab.com/dwt1/wallpapers  
 
-FILE=/home/hughie/.xinitrc
+home='/home/hughie/test'
+
+FILE=$home/.xinitrc
 if test -f "$FILE"; then
     mv $FILE $FILE.bk
-else
-    ln -s /home/hughie/dotfiles/configs/xinitrc /home/hughie/.xinitrc
 fi
+ln -s $home/dotfiles/configs/xinitrc $home/.xinitrc
 
-FILE=/home/hughie/.bashrc
+FILE=$home/.bashrc
 if test -f "$FILE"; then
     mv $FILE $FILE.bk
-else
-    ln -s /home/hughie/dotfiles/configs/bashrc.arch /home/hughie/.bashrc
 fi
+ln -s $home/dotfiles/configs/bashrc.arch $home/.bashrc
 
-FILE=/home/hughie/.bash_aliases
+FILE=$home/.bash_aliases
 if test -f "$FILE"; then
     mv $FILE $FILE.bk
-else
-    ln -s /home/hughie/dotfiles/configs/bash_aliases /home/hughie/.bash_aliases
 fi
+ln -s $home/dotfiles/configs/bash_aliases $home/.bash_aliases
 
-FILE=/home/hughie/.config/i3/config
+FILE=$home/.config/i3/config
 if test -f "$FILE"; then
     mv $FILE $FILE.bk
-else
-    mkdir /home/hughie/.config/i3
-    ln -s /home/hughie/dotfiles/configs/i3.config /home/hughie/.config/i3/config
 fi
+mkdir $home/.config/i3
+ln -s $home/dotfiles/configs/i3.config $home/.config/i3/config
 
-FILE=/home/hughie/.config/i3blocks/i3blocks.conf
+FILE=$home/.config/i3blocks/i3blocks.conf
 if test -f "$FILE"; then
     mv $FILE $FILE.bk
-else
-    mkdir /home/hughie/.config/i3blocks
-    ln -s /home/hughie/dotfiles/configs/i3blocks.conf /home/hughie/.config/i3blocks/i3blocks.conf
-
-    ln -s /home/hughie/dotfiles/i3block-scripts /home/hughie/.config/scripts
 fi
+mkdir $home/.config/i3blocks
+ln -s $home/dotfiles/configs/i3blocks.conf $home/.config/i3blocks/i3blocks.conf
+ln -s $home/dotfiles/i3block-scripts $home/.config/scripts
 
-git clone https://gitlab.com/dwt1/wallpapers  
+FILE=$home/.tmux.conf
+if test -f "$FILE"; then
+    mv $FILE $FILE.bk
+fi
+mkdir $home/.tmux
+ln -s $home/dotfiles/configs/tmux.conf $home/.tmux.conf
